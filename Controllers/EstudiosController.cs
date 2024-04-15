5a75f1534ee4c73a56e4fc590c01176b8cc13d62
Brayan <brayancarrillo014@gmail.com>
2024-04-15 16:41:28 -0500

    Primera versión

---
+++ Controllers/EstudiosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Controllers
+{
+    public class EstudiosController : Controller
+    {
+        private readonly PersonaDbContext _context;
+
+        public EstudiosController(PersonaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Estudios.ToListAsync());
+        }
+    }
+}
---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Controllers
+{
+    public class PersonaController : Controller
+    {
+        private readonly PersonaDbContext _context;
+        public PersonaController(PersonaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Personas.ToListAsync());
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
